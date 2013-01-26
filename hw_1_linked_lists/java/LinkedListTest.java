50be98d7fcdcbb97c3bccd1ebcbf66972b4bd166
Gabe Johnson <gabe.johnson@gmail.com>
2013-01-25 17:29:50 -0700

    Added better text feedback to java tests

--- hw_1_linked_lists/java/LinkedListTest.java
+++ hw_1_linked_lists/java/LinkedListTest.java
@@ -2,6 +2,8 @@ import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.*;
 import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 public class LinkedListTest {
 
@@ -10,7 +12,7 @@ public class LinkedListTest {
 	core.addListener(new RetroPrinter());
 	core.run(LinkedListTest.class);
     }
-
+    
     LinkedList list;
 
     Node buildThreeNodeList(int one, int two, int three) {
@@ -31,17 +33,24 @@ public class LinkedListTest {
 
     @Test 
     public void testInitNode() {
+	System.out.println("testInitNode begin");
 	Node top = list.initNode(1);
 	assertEquals(1, top.value);
 	assertTrue(top.next == null);
+	System.out.println("testInitNode end");
     }
 
     public void expectReport(String actual, String expected) {
-	assertTrue(actual.equals(expected + " ") || actual.equals(expected));
+    	boolean result = actual.equals(expected + " ") || actual.equals(expected);
+	if (!result) {
+	    System.out.println("     -------- they are different!");
+    	}
+	assertTrue("Expected input " + expected + " but got " + actual, result);
     }
 
     @Test 
     public void testReport() {
+	System.out.println("testReport begin");
 	list.appendData(4);
 	list.appendData(2);
 	list.appendData(93);
@@ -51,6 +60,7 @@ public class LinkedListTest {
 
     @Test
     public void testAppendData() {
+	System.out.println("testAppendData begin");
 	list.appendData(4);
 	expectReport(list.report(), "4");
 	list.appendData(2);
@@ -59,10 +69,12 @@ public class LinkedListTest {
 	expectReport(list.report(), "4 2 93");
 	list.appendData(0);
 	expectReport(list.report(), "4 2 93 0");
+	System.out.println("testAppendData end");
     }
 
     @Test 
     public void testAppend() {
+	System.out.println("testAppend begin");
 	list.append(list.initNode(4));
 	expectReport(list.report(), "4");
 	list.append(list.initNode(2));
@@ -71,10 +83,12 @@ public class LinkedListTest {
 	expectReport(list.report(), "4 2 93");
 	list.append(list.initNode(0));
 	expectReport(list.report(), "4 2 93 0");	
+	System.out.println("testAppend end");
     }
 
     @Test 
     public void testInsertData() {
+   	System.out.println("testInsertData begin");
 	list.insertData(0, 10);   // 10
 	expectReport(list.report(), "10");
 	list.insertData(0, 7);    // 7 10
@@ -85,10 +99,12 @@ public class LinkedListTest {
 	expectReport(list.report(), "7 8 9 10");
 	list.insertData(3, 11);   // 7 8 9 11 10   -- thanks Nicholas for finding a bug here
 	expectReport(list.report(), "7 8 9 11 10");
+	System.out.println("testInsertData end");
     }
 
     @Test 
     public void testInsert() {
+	System.out.println("testInsert begin");
 	list.insert(0, list.initNode(10)); // 10
 	expectReport(list.report(), "10");
 	list.insert(0, list.initNode(7)); // 7 10
@@ -99,10 +115,12 @@ public class LinkedListTest {
 	expectReport(list.report(), "7 8 9 10");
 	list.insert(3, list.initNode(11)); // 7 8 9 11 10 -- and here
 	expectReport(list.report(), "7 8 9 11 10");
+	System.out.println("testInsert end");
     }
 
     @Test 
     public void testRemove() {
+	System.out.println("testRemove begin");
 	list.appendData(5);
 	list.appendData(10);
 	list.appendData(15);
@@ -117,28 +135,33 @@ public class LinkedListTest {
 	// remove beginning
 	list.remove(0);
 	expectReport(list.report(), "10");
+	System.out.println("testRemove end");
     }
 
     @Test 
     public void testSize() {
+	System.out.println("testSize begin");
 	assertEquals(0, list.size());
 	list.appendData(5);
 	list.appendData(10);
 	list.appendData(15);
 	list.appendData(20);
-	assertEquals(4, list.size());
+	assertEquals("List size should be 4", 4, list.size());
+	System.out.println("testSize end");
     }
 
     @Test 
     public void testContains() {
+	System.out.println("testContains begin");
 	list.appendData(5);
 	list.appendData(-10);
 	list.appendData(15);
-	assertTrue(list.contains(5));
-	assertTrue(list.contains(-10));
-	assertTrue(list.contains(15));
-	assertFalse(list.contains(45));
-	assertFalse(list.contains(-45));
+	assertTrue("List does not contain 5", list.contains(5));
+	assertTrue("List does not contain -10", list.contains(-10));
+	assertTrue("List does not contain 15", list.contains(15));
+	assertFalse("List should not contain 45", list.contains(45));
+	assertFalse("List should not contain -45", list.contains(-45));
+	System.out.println("testContains end");
     }
 
 
