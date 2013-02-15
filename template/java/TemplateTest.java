ea0e9ed648d23e7cce529e038e6dd7f94ac2acd9
Gabe Johnson <gabe.johnson@gmail.com>
2013-02-15 15:33:39 -0700

    Updated the Java template unit test to be more friendly.

--- template/java/TemplateTest.java
+++ template/java/TemplateTest.java
@@ -1,3 +1,18 @@
+
+// Note on the TemplateTest:
+//
+// You really only need to do two things:
+//
+// 1. Search/replace TemplateTest with the name of your test.
+//
+// 2. Implement a bunch of functions that start with test and end with
+// the name of the test you want. The names should match those
+// provided in ../description-common.json. For example if a test is
+// FooThingWorks, your test function should be testFooThingWorks.
+//
+// Nothing else is required as long as you attach the RetroPrinter to
+// your test runner.
+
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.*;
@@ -8,7 +23,7 @@ public class TemplateTest {
     public static void main(String[] args) {
 	JUnitCore core= new JUnitCore();
 	core.addListener(new RetroPrinter());
-	core.run(LinkedListTest.class);
+	core.run(TemplateTest.class);
     }
 
     @Before
