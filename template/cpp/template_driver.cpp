162737a027fee27fbd98392c0854a21df21004fe
Gabe Johnson <gabe.johnson@gmail.com>
2013-04-07 15:46:22 -0600

    better cpp template

---
+++ template/cpp/template_driver.cpp
@@ -0,0 +1,17 @@
+#include "UTFramework.h"
+#include "template.h"
+
+using namespace Thilenius;
+
+SUITE_BEGIN("Template")
+
+TEST_BEGIN("Template1")
+{
+  //  IsTrue("Check Empty", !any_false(invar), "Default Empty Tree Breaks Invariant");
+}TEST_END
+
+SUITE_END
+
+int main (int argc, char* argv[]) {	
+  UTFrameworkInit;
+}
